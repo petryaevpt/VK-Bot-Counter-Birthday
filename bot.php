8228c4b99242b58b33f7b75fe312d3c405c2fd99
@petryaevpt <gamepetryaev@gmail.com>
2020-01-09 16:58:25 +0300

    Bot Without Checks

---
+++ bot.php
@@ -0,0 +1,56 @@
+<?php
+ini_set("display_errors",1);
+error_reporting(E_ALL);
+
+/*
+Ссылка на группу во ВК, в которой можно проверить бота: https://vk.com/public188328735 
+*/
+
+require_once 'config.php';
+require_once 'functions.php';
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+$mysqli = new mysqli("localhost", "root", "root", "petryaev_bd_counter_bot");
+if ($mysqli->connect_error)
+{
+  die("Connection failed: " . $mysqli->connect_error);
+}
+
+switch ($data['type'])
+{
+  case 'confirmation':
+    echo $confirmation_token;
+  break;
+
+  case 'message_new':
+  $message = $data['object']['text'];
+  $peer_id = $data['object']['peer_id'] ?: $data['object']['user_id'];
+
+  $user_info = users_get($access_token, $peer_id)[0];
+  $user_name = $user_info['first_name'];
+
+  $validate_message = conversionDate($message);
+
+  if (($validate_message != 0) && (chekDate($validate_message)))
+  {
+    messages_send($group_token, $peer_id,
+    "Хорошо, понял. Теперь ты сможешь узнать процент.");
+    insertOrUpdate($mysqli, $peer_id, $validate_message);
+  } elseif ($message == 'Процент')
+    {
+      $percent = getPercent($mysqli, $peer_id);
+      messages_send($group_token, $peer_id, "{$percent}");
+    } else
+      {
+         messages_send($group_token, $peer_id,
+         "{$user_name}, напиши, когда у тебя был ПОСЛЕДИЙ день рождения в формате ДД.ММ.ГГГГ. Если уже написал дату, пиши «Процент».");
+      }
+
+    echo('ok');
+  break;
+
+  default:
+    echo('ok');
+  break;
+}
---
+++ functions.php
@@ -0,0 +1,146 @@
+<?php
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+//Отправляет сообщение
+function messages_send($group_token, $peer_id, $message)
+{
+  api($group_token, 'messages.send', array(
+  'peer_id' => $peer_id,
+  'message' => $message,
+  'random_id' => rand(999999,9999999),
+   ));
+}
+
+//Возвращает расширенную информацию о пользователях
+function users_get($group_token, $user_id)
+{
+  return api($group_token, 'users.get', array(
+    'user_ids' => $user_id,
+    'fields' => 'bdate',
+    ));
+}
+
+//Callback API
+function api($access_token, $method, $params)
+{
+  $params['access_token'] = $access_token;
+  $params['v'] = '5.100';
+  $query = http_build_query($params);
+  $url = 'https://api.vk.com/method/' . $method . '?' . $query;
+
+  $curl = curl_init($url);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  $json = curl_exec($curl);
+  $error = curl_error($curl);
+  if ($error)
+  {
+    error_log($error);
+    throw new Exception("Неудачный запрос: {$method}.");
+  }
+  curl_close($curl);
+
+  $response_api = json_decode($json, true);
+  if (!$response_api || !isset($response_api['response']))
+  {
+    error_log($json);
+    throw new Exception("Неверный ответ на запрос: {$method}.");
+  }
+  return $response_api['response'];
+}
+
+//Получаем процент с момента ДР
+function getPercent($mysqli, $user_id)
+{
+  $sql = "SELECT birthday_date FROM users_bday WHERE users_id = '{$user_id}';";
+  $result = $mysqli->query($sql);
+
+  if ($result->num_rows != 0)
+  {
+    while ($row = $result->fetch_assoc())
+    {
+      $days_in_year = date('L') ? 366 : 365;
+
+      $now_date = new DateTime();
+      $birthday_date = $row['birthday_date'];
+      $birthday = new DateTime($birthday_date);
+
+      $diff = $birthday->diff($now_date);
+      $diff_days = $diff->days;
+
+      $percent_raw = $diff_days/$days_in_year;
+      $percent = round($percent_raw * 100);
+
+      return sprintf("Ого, с твоего Дня рождения прошло %d%% года.", $percent);
+    }
+  } else
+    {
+      return 'Сначала напиши дату в формате ГГГГ-ММ-ДД.';
+    }
+}
+
+//Проверка на дату: должна быть не раньше, чем год назад + не позже, чем сегодня
+function chekDate($bdate)
+{
+  $days_in_year = date('L') ? 366 : 365;
+  $datetime = new DateTime();
+  $d = new DateTime($bdate);
+  $diff = $d->diff($datetime);
+
+  $date_unix = strtotime(date('Y-m-d'));
+  $bdate_unix = strtotime($bdate);
+
+  $diff_days = $diff->days;
+  if (($diff_days < $days_in_year) && ($bdate_unix < $date_unix))
+  {
+    return 1;
+  }
+    return 0;
+}
+
+//Заполнение базы данных
+function insertOrUpdate($mysqli, $user_id, $message)
+{
+  $select = "SELECT birthday_date FROM users_bday WHERE users_id = '{$user_id}';";
+  $result = $mysqli->query($select);
+
+  //Если есть поле с таким $user_id, перезаписываем дату, иначе создаём поле
+  if($result->num_rows != 0)
+  {
+  $update = "UPDATE users_bday SET birthday_date='{$message}' WHERE users_id = {$user_id};";
+  $mysqli->query($update);
+  } else
+    {
+    $insert = "INSERT INTO users_bday (users_id, birthday_date) VALUES ('{$user_id}', '{$message}');";
+    $mysqli->query($insert);
+    }
+}
+
+function conversionDate($message)
+{
+  if (strlen($message) != 10)
+  {
+    return 0;
+  }
+
+  $day = mb_substr($message, 0, 2, 'utf-8');
+  $month = mb_substr($message, 3, 2, 'utf-8');
+  $year = mb_substr($message, 6, 4, 'utf-8');
+
+  if (($day == 29) && ($month == 02))
+  {
+    $day = 28;
+  }
+
+  $date = $year . '-' . $month . '-' . $day;
+
+  $format = 'Y-m-d';
+  $datetime = DateTime::createFromFormat($format, $date);
+
+  if ($datetime->format($format) != $date)
+  {
+    return 0;
+  }
+
+  return $date;
+}
